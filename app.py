8cd39d2648a625bf4e06b35a472101a22618c27a
nirmal1311 <regsite1311@gmail.com>
2024-07-23 10:34:50 +0530

    add text

--- app.py
+++ app.py
@@ -41,7 +41,8 @@ def input_pdf_setup(uploaded_file):
 ## Streamlit App
 
 st.set_page_config(page_title="ATS Resume EXpert")
-st.header("Application Tracking System",body='by Nirmal Avhad',divider='rainbow')
+st.header("Application Tracking System",divider='rainbow')
+st.text("Nirmal Avhad")
 input_text=st.text_area("Job Description: ",key="input")
 uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
 
